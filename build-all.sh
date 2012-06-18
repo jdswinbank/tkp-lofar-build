e78f9114422f1edf3ee79285664d1402e856d78d
John Swinbank <swinbank@transientskp.org>
2012-06-18 11:54:53 +0200

    Add casapy to path

--- build-all.sh
+++ build-all.sh
@@ -182,6 +182,9 @@ export PATH=$TARGET/cookbook\${PATH:+:\${PATH}}
 . $LOFAR_TARGET/lofarinit.sh
 # Required to use gsm.py on heastro1
 export PYTHONPATH=/opt/monetdb/lib/python2.6/site-packages\${PYTHONPATH:+:\${PYTHONPATH}}
+
+# Casapy
+export PATH=/opt/casapy${PATH:+:${PATH}}
 END
 
 # Install this build as the default
